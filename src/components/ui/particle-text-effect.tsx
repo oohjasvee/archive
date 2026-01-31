9bc2b348f0ede688b925b6fe0be5c70524921d13
Varad Srivastava (Services) <varadsrivastava7@gmail.com>
2026-01-31 10:52:40 +0000

    further increase

--- src/components/ui/particle-text-effect.tsx
+++ src/components/ui/particle-text-effect.tsx
@@ -181,7 +181,7 @@ export function ParticleTextEffect({ words = DEFAULT_WORDS }: ParticleTextEffect
 
     // Draw text
     offscreenCtx.fillStyle = "white"
-    offscreenCtx.font = "bold 140px 'Playfair Display', serif"
+    offscreenCtx.font = "bold 180px 'Playfair Display', serif"
     offscreenCtx.textAlign = "center"
     offscreenCtx.textBaseline = "middle"
     offscreenCtx.fillText(word, canvas.width / 2, canvas.height / 2)
